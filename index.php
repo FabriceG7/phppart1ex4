25c4c71cadfc779121ac6eb8af0a3e6aead9b35a
FabriceG7 <goret.goret@sfr.fr>
2020-05-22 14:21:55 +0200

    phppartit1ex4

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$name = 'Fabrice';
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>php part1 ex4</title>
+  </head>
+  <body>
+    <h1>Bonjour <?= $name; ?>, comment vas-tu?</h1>
+  </body>
+</html>
